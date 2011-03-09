929f736d571f3c1b97fe3c51077c16c3edaf9f91
Patrick Pfeiffer <patrick@buzzle.at>
2011-03-09 14:00:40 +0100

    some stylus-magic

--- public/client.js
+++ public/client.js
@@ -14,7 +14,7 @@
           , rndB = Math.floor(Math.random()*10)
           , colors = ['#dd0','#d0d','#0dd','#d00','#0d0','#00d']
         if (!this.get('x')) this.set({x:(rndA*50)})
-        if (!this.get('y')) this.set({y:(rndB*50)})
+        if (!this.get('y')) this.set({y:(rndB*50)+120})
         if (!this.get('w')) this.set({w:(rndA*5)+20})
         if (!this.get('h')) this.set({h:(rndA*5)+20})
         if (!this.get('c')) this.set({c:colors[rndA%colors.length]})
